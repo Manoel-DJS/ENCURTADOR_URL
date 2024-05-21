1f8830ba86b54411397fee53e76a80515e0061af
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-05-21 17:05:28 -0300

    create method getMapping - commit final

--- src/main/java/tech/buildrun/urlshort/controller/UrlController.java
+++ src/main/java/tech/buildrun/urlshort/controller/UrlController.java
@@ -3,15 +3,16 @@ package tech.buildrun.urlshort.controller;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tech.buildrun.urlshort.controller.dto.ShortenUrlRequest;
 import tech.buildrun.urlshort.controller.dto.ShortenUrlResponse;
 import tech.buildrun.urlshort.entity.UrlEntity;
 import tech.buildrun.urlshort.repository.UrlRepository;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.Random;
 
@@ -36,6 +37,22 @@ public class UrlController {
         var redirectUrl = servletRequest.getRequestURL().toString().replace("shorten-url", id);
 
         return ResponseEntity.ok(new ShortenUrlResponse(redirectUrl));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Void> redirect(@PathVariable("id") String id){
+
+        var url = urlRepository.findById(id);
+
+        // Se a URL for vazia
+        if(url.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create(url.get().getFullUrl()));
+
+        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
     }
 
 }
