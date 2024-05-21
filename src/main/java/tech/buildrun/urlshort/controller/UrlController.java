ccc537689502c46df5d5c4587e5634488bdea282
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-05-21 16:19:09 -0300

    PostMapping OK

--- src/main/java/tech/buildrun/urlshort/controller/UrlController.java
+++ src/main/java/tech/buildrun/urlshort/controller/UrlController.java
@@ -1,18 +1,41 @@
 package tech.buildrun.urlshort.controller;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import tech.buildrun.urlshort.controller.dto.ShortenUrlRequest;
+import tech.buildrun.urlshort.controller.dto.ShortenUrlResponse;
+import tech.buildrun.urlshort.entity.UrlEntity;
+import tech.buildrun.urlshort.repository.UrlRepository;
+
+import java.time.LocalDateTime;
+import java.util.Random;
 
 @RestController
 public class UrlController {
 
-    @PostMapping(value = "/shorten-url")
-    public ResponseEntity<Void> shortenUrl(@RequestBody ShortenUrlRequest request){
+    private final UrlRepository urlRepository;
 
-        return ResponseEntity.ok().build();
+    public UrlController(UrlRepository urlRepository) {
+        this.urlRepository = urlRepository;
+    }
+
+    @PostMapping(value = "/shorten-url")
+    public ResponseEntity<ShortenUrlResponse> shortenUrl(@RequestBody ShortenUrlRequest request, HttpServletRequest servletRequest){
+        String id;
+        do {
+            id = RandomStringUtils.randomAlphanumeric(5, 10);
+        } while (urlRepository.existsById(id));
+
+        urlRepository.save(new UrlEntity(id, request.url(), LocalDateTime.now().plusMinutes(1)));
+
+        var redirectUrl = servletRequest.getRequestURL().toString().replace("shorten-url", id);
+
+        return ResponseEntity.ok(new ShortenUrlResponse(redirectUrl));
     }
 
 }
---
+++ src/main/java/tech/buildrun/urlshort/controller/dto/ShortenUrlResponse.java
@@ -0,0 +1,4 @@
+package tech.buildrun.urlshort.controller.dto;
+
+public record ShortenUrlResponse(String url) {
+}
