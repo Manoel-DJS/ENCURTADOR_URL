6269689e6f034eeeff1e6de75da5906626913225
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2025-04-09 09:39:55 -0300

    feat - create method shortenUrl in UrlService

--- src/main/java/tech/buildrun/urlshort/service/UrlService.java
+++ src/main/java/tech/buildrun/urlshort/service/UrlService.java
@@ -1,7 +1,14 @@
 package tech.buildrun.urlshort.service;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
+import tech.buildrun.urlshort.controller.dto.ShortenUrlRequest;
+import tech.buildrun.urlshort.controller.dto.ShortenUrlResponse;
+import tech.buildrun.urlshort.entity.UrlEntity;
 import tech.buildrun.urlshort.repository.UrlRepository;
+
+import java.time.LocalDateTime;
 
 @Service
 public class UrlService {
@@ -11,4 +18,13 @@ public class UrlService {
         this.urlRepository = urlRepository;
     }
 
+    public ShortenUrlResponse shortenUrl(ShortenUrlRequest request, HttpServletRequest servletRequest){
+        String id;
+        do {
+            id = RandomStringUtils.randomAlphanumeric(5, 10);
+        } while (urlRepository.existsById(id));
+        urlRepository.save(new UrlEntity(id, request.url(), LocalDateTime.now().plusMinutes(2)));
+        var redirectUrl = servletRequest.getRequestURL().toString().replace("shorten-url", id);
+        return new ShortenUrlResponse(redirectUrl);
+    }
 }
